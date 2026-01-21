2b97585f3fd825ba5d5c2c69210ae5213bd1e37f
Eldon Peterson <eldonpeterson@Eldons-MacBook-Pro.local>
2026-01-21 11:42:29 -0600

    Fix local dev server binding for macOS

--- server/index.ts
+++ server/index.ts
@@ -160,11 +160,8 @@ app.use((req, res, next) => {
   }
 
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
+  const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1';
+  server.listen(port, host, () => {
     log(`serving on port ${port}`);
   });
 })();
